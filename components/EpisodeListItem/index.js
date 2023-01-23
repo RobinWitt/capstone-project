1d694f5e7ba8885efabf8d20b6e9b436d2bf093a
Robin Witt <robin.w@posteo.de>
2023-01-23 16:25:01 +0100

    fix clickable list item

--- components/EpisodeListItem/index.js
+++ components/EpisodeListItem/index.js
@@ -2,22 +2,25 @@ import Link from "next/link";
 import styled from "styled-components";
 
 const StyledListItem = styled.li`
-  display: flex;
-  gap: 2rem;
   background-color: whitesmoke;
   margin: 0.5rem;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
   padding: 0.5rem;
+  width: 100%;
   border: 1px solid;
   border-radius: 5px;
 `;
 
 export default function EpisodeListItem({ nummer, titel, href }) {
   return (
-    <Link href={href}>
-      <StyledListItem>
+    <StyledListItem>
+      <StyledLink href={href}>
         <p>#{nummer}</p>
         <p>...{titel}</p>
-      </StyledListItem>
-    </Link>
+      </StyledLink>
+    </StyledListItem>
   );
 }
