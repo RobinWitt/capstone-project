671439b345cc0e48a83499b811ac826fd91d6c97
Robin Witt <robin.w@posteo.de>
2023-01-24 16:50:32 +0100

    cut onClick function

--- components/EpisodeDetails/index.js
+++ components/EpisodeDetails/index.js
@@ -62,7 +62,7 @@ export default function EpisodeDetails({ episode }) {
 
   return (
     <StyledArticle>
-      <StyledReturnButton type="button" onClick={() => router.back()}>
+      <StyledReturnButton type="button" onClick={router.back}>
         <ReturnIcon />
       </StyledReturnButton>
       <StyledDetailsTitle>Folge {number}</StyledDetailsTitle>
