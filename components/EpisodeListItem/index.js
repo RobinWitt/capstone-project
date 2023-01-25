ffb94dbf45d9e5ec63d03438b6cc2ddc44c2d24a
Robin Witt <robin.w@posteo.de>
2023-01-25 12:35:07 +0100

    global state & display favorites in overview

--- components/EpisodeListItem/index.js
+++ components/EpisodeListItem/index.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styled from "styled-components";
+import SVGIcon from "../Icons";
 
 const StyledListItem = styled.li`
   background-color: whitesmoke;
@@ -8,6 +9,7 @@ const StyledListItem = styled.li`
 
 const StyledEpisodeLink = styled(Link)`
   display: flex;
+  justify-content: space-between;
   padding: 0.5rem;
   width: 100%;
   border: 2px solid;
@@ -16,13 +18,17 @@ const StyledEpisodeLink = styled(Link)`
   color: darkgreen;
 `;
 
-export default function EpisodeListItem({ nummer, titel, href }) {
+export default function EpisodeListItem({ number, title, href, isFaved }) {
   return (
     <StyledListItem>
       <StyledEpisodeLink href={href}>
         <p>
-          #{nummer} ...{titel}
+          #{number} ...{title}
         </p>
+        <SVGIcon
+          variant={isFaved ? "favoriteFilled" : "favoriteEmpty"}
+          width="20px"
+        />
       </StyledEpisodeLink>
     </StyledListItem>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import EpisodeList from "@/components/EpisodeList";
 import EpisodeListItem from "@/components/EpisodeListItem";
 import useSWR from "swr";
+import { useAtom } from "jotai";
+import { initialFavorites } from "@/components/Favorites/InitialFavorites";
+import handleFavorites from "@/components/Favorites/FavoriteHandler";
 
 // this will change when using remote API
 const URL = "/serie.json";
 
 export default function HomePage() {
   const { data, error, isLoading } = useSWR(URL);
+  const [favorites] = useAtom(initialFavorites);
 
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
@@ -21,9 +25,10 @@ export default function HomePage() {
               return (
                 <EpisodeListItem
                   key={number}
-                  nummer={number}
-                  titel={title}
+                  number={number}
+                  title={title}
                   href={`/episodes/${number}`}
+                  isFaved={handleFavorites(favorites, number.toString())}
                 />
               );
             })}
