cbb43afd9d0cad77970647dd6e3ac1020709473c
Robin Witt <robin.w@posteo.de>
2023-01-30 12:36:33 +0100

    add db schema: Episode

---
+++ db/models/Episode.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const EpisodeSchema = new Schema({
+  nummer: { Number, required: true },
+  titel: { String, required: true },
+  autor: String,
+  hörspielskriptautor: String,
+  beschreibung: String,
+  veröffentlichungsdatum: String,
+  kapitel: [{ titel: String }],
+  sprecher: [[String, String]],
+  links: {
+    json: String,
+    cover: String,
+    cover_itunes: String,
+    cover_kosmos: String,
+  },
+  teile: [
+    {
+      teilNummer: Number,
+      buchstabe: String,
+      titel: String,
+      autor: String,
+      beschreibung: String,
+      kapitel: [{ titel: String }],
+      sprecher: [[String, String]],
+    },
+  ],
+});
+
+const Episode =
+  mongoose.models.Location || mongoose.model("Episode", EpisodeSchema);
+
+export default Episode;
