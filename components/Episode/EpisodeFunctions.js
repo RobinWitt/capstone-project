2191593634cf8321f755742ec9554f97cbe1416a
RobinWitt <reg.rw@posteo.de>
2023-03-29 12:57:22 +0200

    fix cover urls

--- components/Episode/EpisodeFunctions.js
+++ components/Episode/EpisodeFunctions.js
@@ -1,9 +1,7 @@
 export function getCoverURL(links) {
   if (links) {
-    const { cover_kosmos, cover_itunes, cover } = links;
-    if (cover_kosmos) {
-      return cover_kosmos;
-    } else if (cover_itunes) {
+    const { cover_itunes, cover } = links;
+    if (cover_itunes) {
       return cover_itunes;
     } else if (cover) {
       return cover;
